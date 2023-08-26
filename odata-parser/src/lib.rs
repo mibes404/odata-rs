4e03f5b4959e23c7b4d60a2adbfd880eb6fc278e
Marcel <mibes@avaya.com>
2023-08-26 14:59:15 +0200

    Usage sample

--- odata-parser/src/lib.rs
+++ odata-parser/src/lib.rs
@@ -2,6 +2,17 @@ use odata_model::{error::ODataResult, ODataResource};
 
 pub use odata_model;
 
+/// Parse an OData v4 request Url
+/// ```rust
+/// use odata_parser::parse_url;
+/// use odata_model::{ODataResource, ODataResourceKind};
+///
+/// let url = "http://services.odata.org/V4/TripPinService/People('O''Neil')";
+/// let resource = parse_url(url).expect("Failed to create a resource from the URL");
+///
+/// assert_eq!(resource.name, "People");
+/// assert_eq!(resource.key, Some("O'Neil".to_string()))
+/// ```
 pub fn parse_url(url: &str) -> ODataResult<ODataResource> {
     let resource = ODataResource::try_from(url)?;
     Ok(resource)
